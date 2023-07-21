e47b6c853c617803203216a871556aae3202294e
SendToSouthEast <SendToSouthEast@outlook.com>
2023-07-21 16:03:26 +0800

    feat(errcode):修复错误码部分一些问题，增加了计数警告，增加注释

--- src/errcode/errcode.py
+++ src/errcode/errcode.py
@@ -1,21 +1,20 @@
-from enum import Enum, auto
 
-# 错误码
+# 错误码:[提示信息,触发警告需要的次数(每n次警告一次)]
 codes = {
-    'Normal': '一切正常',
-    'EMOJI_NOT_EXIST': '表情不存在',
-    'IMG_NOT_EXIST': '图片不存在',
-    'MARKETFACE_NOT_EXIST': '大表情不存在',
-    'IMG_DESERIALIZATION_ERROR': '图片反序列化错误',
-    'IMG_UNKNOWN_TYPE': '图片类型未知',
-    'MIXMSG_DESERIALIZATION_ERROR': '混合消息反序列化错误',
-    'EXTSTR_NOT_EXIST_TARGET': 'EXTSTR不存在目标',
-    'EXTSTR_JSON_ERR_DECODE': 'EXTSTR JSON解码错误',
-    'IO_ERROR': 'IO读取写入错误',
-    'JAVA_DESER_ERR_INPUT_TYPE': 'Java反序列化错误  输入类型错误',
-    'JAVA_DESER_JSON_ERR_DECODE': 'Java反序列化错误 - JSON解码错误'
-
+    'Normal': ['一切正常', 0],
+    'EMOJI_NOT_EXIST': ['表情不存在', 100],
+    'IMG_NOT_EXIST': ['图片不存在', 100],
+    'MARKETFACE_NOT_EXIST': ['大表情不存在', 100],
+    'IMG_DESERIALIZATION_ERROR': ['图片反序列化错误', 100],
+    'IMG_UNKNOWN_TYPE': ['图片类型未知', 100],
+    'MIXMSG_DESERIALIZATION_ERROR': ['混合消息反序列化错误', 100],
+    'EXTSTR_NOT_EXIST_TARGET': ['EXTSTR不存在目标', 100],
+    'EXTSTR_JSON_ERR_DECODE': ['EXTSTR JSON解码错误', 100],
+    'IO_ERROR': ['IO读取写入错误', 100],
+    'JAVA_DESER_ERR_INPUT_TYPE': ['Java反序列化错误  输入类型错误', 100],
+    'JAVA_DESER_JSON_ERR_DECODE': ['Java反序列化错误 - JSON解码错误', 100]
 }
+
 
 
 class err_code():
@@ -23,7 +22,7 @@ class err_code():
         self.mode = mode
         #日志等级
         self.LOG_LEVEL_INFO = 0
-        self.LOG_LEVEL_WARNING = 1
+        self.LOG_LEVEL_WARN = 1
         self.LOG_LEVEL_ERR = 2
 
         self.logLevel = self.LOG_LEVEL_WARNING
@@ -35,10 +34,22 @@ class err_code():
 
     #设置日志等级
     def setLogLevel(self, logLevel):
+        """设置日志等级
+
+        :param logLevel: 日志等级
+        :return: 无
+        """
         self.logLevel = logLevel
 
 
     def log(self, tag, logLevel, info):
+        """输出日志信息
+
+        :param tag: 发送者标签
+        :param logLevel: 日志等级
+        :param info: 信息
+        :return: 无
+        """
         if logLevel >= self.logLevel:
             if self.mode == "print":
                 print(tag + " :" + "errinfo")
@@ -46,6 +57,12 @@ class err_code():
                 pass
 
     def parse_err(self, code, errdata):
+        """
+
+        :param code:
+        :param errdata:
+        :return:
+        """
         if code not in codes:
             print(f"code not in codes! code is {code}")
             return {}
@@ -59,10 +76,22 @@ class err_code():
 
         err = {
             "s": False,
-            "errinfo": codes[code] + f"相关信息：{errdata}"
+            "errinfo": codes[code][0] + f"相关信息：{errdata}"
 
         }
-        return {}
+        return err
+
+    def count_check(self, code):
+        """检查错误码有没有过量，及时弹出警告信息
+        要求输入code是为了节省性能，只在某code的数量改变时检查
+
+        :param code:错误码
+        :return:无
+        """
+        if self.counts[code] % codes[code][1] == 0:
+            errinfo = f"解析时已经发生{self.counts[code]}次“{codes[code][0]}”的错误，请检查你的设置"
+            self.log("parse_count", self.LOG_LEVEL_WARN, errinfo)
+
 
 
 
